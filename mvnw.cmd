c686ba1ed9606a99594dbc4d3d30cb4d3911d879
P-Kefhitilwe <cse24-096@thuto.bac.ac.bw>
2025-11-07 13:53:25 +0200

    Delete mvnw.cmd

--- mvnw.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-set MAVEN_WRAPPER_HOME=%~dp0\.mvn\wrapper
-java -jar "%MAVEN_WRAPPER_HOME%\maven-wrapper.jar" %*
-@echo off
-setlocal
-set MVNW_HOME=%~dp0.mvn\wrapper
-java -jar "%MVNW_HOME%\maven-wrapper.jar" %*
